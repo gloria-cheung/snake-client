0ad8226d96f4e9ad4ec8dd7967afa04bf858cca4
Gloria <gloriacheung812@gmail.com>
2022-04-19 15:15:39 +0000

    refactor and move input code to separate file

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
-const connect = require("./client")
+const connect = require("./client");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  // "data" callback handler for stdin
-  const handleUserInput = function(key) {
-    // ctrl + c to exit the game
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
 setupInput();
