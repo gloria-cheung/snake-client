7af39d1684420f527e3351694273abe6e43fd9b5
Gloria <gloriacheung812@gmail.com>
2022-04-19 17:03:29 +0000

    refactor code by creating constants module for variables that won't change

---
+++ constants.js
@@ -0,0 +1,21 @@
+const IP = "localhost";
+const PORT = 50541;
+const MOVE_UP_KEY = "Move: up";
+const MOVE_DOWN_KEY = "Move: down";
+const MOVE_LEFT_KEY = "Move: left";
+const MOVE_RIGHT_KEY = "Move: right";
+const messages = {
+  h: "hi stranger!",
+  l: "lolololol",
+  o: "oh no..."
+}
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MOVE_UP_KEY,
+  messages
+}
