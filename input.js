a850d3de3415fe39191f587239a3406b41061e54
Gloria <gloriacheung812@gmail.com>
2022-04-19 16:39:31 +0000

    eslint file for semicolons

--- input.js
+++ input.js
@@ -21,29 +21,29 @@ const handleUserInput = function(key) {
   // ctrl + c to exit the game
   if (key === "\u0003") {
     process.exit();
-  };
+  }
   if (key === "w") {
-    // since connection object is named connection (instead of conn), use connection.write to write to server  
+    // since connection object is named connection (instead of conn), use connection.write to write to server
     connection.write("Move: up");
-  };
+  }
   if (key === "s") {
     connection.write("Move: down");
-  };
+  }
   if (key === "a") {
     connection.write("Move: left");
-  };
+  }
   if (key === "d") {
     connection.write("Move: right");
-  };
+  }
   if (key === "h") {
     connection.write("Say: hi stranger!");
-  };
+  }
   if (key === "o") {
     connection.write("Say: oh no..");
-  };
+  }
   if (key === "l") {
     connection.write("Say: lolololol");
-  };
+  }
 };
 
 module.exports = { setupInput };
