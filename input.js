64f2b01cca8429379587518c8224edc36df0f6da
Gloria <gloriacheung812@gmail.com>
2022-04-19 16:34:12 +0000

    pass connection object into input func and add event listener for key press of w,s,a,d and write to server to make snake move

--- input.js
+++ input.js
@@ -1,11 +1,17 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => {
+    handleUserInput(key);
+  });
 
   return stdin;
 };
@@ -13,9 +19,22 @@ const setupInput = function() {
 // "data" callback handler for stdin
 const handleUserInput = function(key) {
   // ctrl + c to exit the game
-  if (key === '\u0003') {
+  if (key === "\u0003") {
     process.exit();
-  }
+  };
+  if (key === "w") {
+    // since connection object is named connection (instead of conn), use connection.write to write to server  
+    connection.write("Move: up");
+  };
+  if (key === "s") {
+    connection.write("Move: down");
+  };
+  if (key === "a") {
+    connection.write("Move: left");
+  };
+  if (key === "d") {
+    connection.write("Move: right");
+  };
 };
 
 module.exports = { setupInput };
