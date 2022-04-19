9381c809bd536736d0700a8ef89144db61e4e08a
Gloria <gloriacheung812@gmail.com>
2022-04-19 15:15:01 +0000

    move setupInput func from play.js to input.js and require module

---
+++ input.js
@@ -0,0 +1,21 @@
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+// "data" callback handler for stdin
+const handleUserInput = function(key) {
+  // ctrl + c to exit the game
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = setupInput;
