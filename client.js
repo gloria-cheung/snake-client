ee05e001e862cb2bee17aeca897d4ddc28439586
Gloria <gloriacheung812@gmail.com>
2022-04-19 14:14:12 +0000

    move connection code from play.js to client.js and require module

---
+++ client.js
@@ -0,0 +1,26 @@
+// establishes a connection with the game server
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost" , // IP address here,
+    port: 50541 // PORT number here,
+  });
+  conn.on("connect", () => {
+    // print msg when connection is first established
+    console.log("YAY! Connected to server 😄");
+  })
+  conn.on("data", (data) => {
+    // message that server sends back to us
+    console.log("Server says:", data);
+  })
+  conn.on("end", () => {
+    // print msg when connection is terminated 
+    console.log("BOO, disconnected from server 😣")
+  })
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,28 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost" , // IP address here,
-    port: 50541 // PORT number here,
-  });
-  conn.on("connect", () => {
-    // print msg when connection is first established
-    console.log("YAY! Connected to server 😄");
-  })
-  conn.on("data", (data) => {
-    // message that server sends back to us
-    console.log("Server says:", data);
-  })
-  conn.on("end", () => {
-    // print msg when connection is terminated 
-    console.log("BOO, disconnected from server 😣")
-  })
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const connect = require("./client")
 
 console.log("Connecting ...");
 connect();
